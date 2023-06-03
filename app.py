8cf0186e3a47f3d6abb1a27ef4a2002b13424b0a
Crystal <chi200706@gmail.com>
2023-06-03 16:02:03 +0800

    add a get method test

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
     return "HiHi"
+
+
+@app.route('/gettest', methods=['GET'])
+def get_test():
+    string = request.args.get('string')
+    return "您輸入的是：" + string
